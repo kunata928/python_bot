72fb6d87e9d82ca43c45eddade1f4d2aa7789405
Natali <kunata928@gmail.com>
2022-03-29 14:42:36 +0300

    return_rates fixed

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ import requests
 TOKEN = "2032032327:AAFoTSbbdRkdcZTD4Q2ASsX3nuUWobzJW8M"
 
 url = 'https://openexchangerates.org/api/latest.json?app_id=1220e55ab6db41adb27d87036ec6dd40'
-data = requests.get(url) # requests data from API
-data = data.json() # converts return data to json
+# data = requests.get(url) # requests data from API
+# data = data.json() # converts return data to json
 
 
 WEATHER_URL = 'http://api.weatherstack.com/current?access_key=11c620b45d1e94840308f10d8fc44724'
-WEATHER_PARAMS0 = {'query':'Istanbul'}#Cape Town
-WEATHER_PARAMS1 = {'query':'Krakow'}#Cape Town
-WEATHER_PARAMS2 = {'query':'Moskva'}#Cape Town
+WEATHER_PARAMS0 = {'query':'Istanbul'}
+WEATHER_PARAMS1 = {'query':'Krakow'}
+WEATHER_PARAMS2 = {'query':'Moskva'}
 
 
 
 # print(weather.json()['current']['temperature']) # will print only the temperature; print without indexing to see all the values returned!
 # Retrieve values from API
 
-pln_rate = data['rates']['PLN']
-eur_rate = data['rates']['EUR']
-rub_rate = data['rates']['RUB']
-try_rate = data['rates']['TRY']
+# pln_rate = data['rates']['PLN']
+# eur_rate = data['rates']['EUR']
+# rub_rate = data['rates']['RUB']
+# try_rate = data['rates']['TRY']
 
 def return_weather():
     weather0 = requests.get(WEATHER_URL, params=WEATHER_PARAMS0)
@@ -37,6 +37,12 @@ def return_weather():
 
 
 def return_rates():
+    data = requests.get(url)  # requests data from API
+    data = data.json()
+    pln_rate = data['rates']['PLN']
+    eur_rate = data['rates']['EUR']
+    rub_rate = data['rates']['RUB']
+    try_rate = data['rates']['TRY']
     return "Hello. Today, USD conversion rates are as follows: USD->PLN = "+str(pln_rate)+", USD->EUR = "+str(eur_rate)+\
            ", USD->RUB = "+str(rub_rate)+", USD->TRY = "+str(try_rate)
 
@@ -55,6 +61,7 @@ def start(message, bot):
 
 def main():
     bot = telebot.TeleBot(TOKEN)  # You can set parse_mode by default. HTML or MARKDOWN
+
 
     @bot.message_handler(commands=['start', 'help'])
     def send_welcome(message):
