04f662020f77506e1fca8c1bd333fd121981d8a5
Natali <kunata928@gmail.com>
2022-04-15 16:51:57 +0300

    add boto s3connection

--- settings.py
+++ settings.py
@@ -1,11 +1,9 @@
-heroku = 1
 import os
 
+heroku = 1
 if heroku:
     from boto.s3.connection import S3Connection
-    TOKEN_TG_BOT = S3Connection(os.environ['TOKEN_TG_BOT'])
-    TOKEN_EXCHANGE = S3Connection(os.environ['TOKEN_EXCHANGE'])
-    TOKEN_WEATHER = S3Connection(os.environ['TOKEN_WEATHER'])
+    s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 else:
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     if os.path.exists(dotenv_path):
