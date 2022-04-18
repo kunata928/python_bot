c7c7d960f2745554a1ae55c140e76ae969a7e9fc
Natali <kunata928@gmail.com>
2022-04-18 16:51:54 +0300

    without env vars

--- settings.py
+++ settings.py
@@ -1,26 +1,11 @@
 import os
-
-# heroku = 1
 from boto.s3.connection import S3Connection
+
 s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+
 TOKEN_TG_BOT = os.environ['TOKEN_TG_BOT']
 TOKEN_EXCHANGE = os.environ['TOKEN_EXCHANGE']
 TOKEN_WEATHER = os.environ['TOKEN_WEATHER']
-
-# if heroku:
-#     from boto.s3.connection import S3Connection
-#     s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
-#     TOKEN_TG_BOT = os.environ['TOKEN_TG_BOT']
-#     TOKEN_EXCHANGE = os.environ['TOKEN_EXCHANGE']
-#     TOKEN_WEATHER = os.environ['TOKEN_WEATHER']
-# else:
-#     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-#     if os.path.exists(dotenv_path):
-#         from dotenv import load_dotenv
-#         load_dotenv(dotenv_path=dotenv_path)
-#     TOKEN_TG_BOT = os.environ.get("TOKEN_TG_BOT")
-#     TOKEN_EXCHANGE = os.environ.get("TOKEN_EXCHANGE")
-#     TOKEN_WEATHER = os.environ.get("TOKEN_WEATHER")
 
 EXCHANGE_URL = 'https://openexchangerates.org/api/latest.json?app_id='+TOKEN_EXCHANGE
 WEATHER_URL = 'http://api.weatherstack.com/current?access_key='+TOKEN_WEATHER
